6f047a7c99a945ecd87763b79fbc00d617a56e1c
Matt Morgis <matt@rappiddevelopment.com>
2016-11-05 17:39:41 -0400

    test: creating the main test file

---
+++ test/test.js
@@ -0,0 +1,18 @@
+/*
+ * This file is part of quickbooks-js
+ * https://github.com/RappidDevelopment/quickbooks-js
+ *
+ * Based on qbws: https://github.com/johnballantyne/qbws
+ *
+ * (c) 2015 johnballantyne
+ * (c) 2016 Rappid Development LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+
+});
